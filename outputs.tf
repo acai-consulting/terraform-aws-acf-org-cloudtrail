0ac8a44eb4657f71f412530faa056970de1ebd6b
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-02-26 17:52:08 +0100

    fix

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,7 @@
 
 output "core_configuration_to_write" {
   description = "This must be in sync with the Account Baselining"
-  value = var.core_configuration_cluster_name == "" ? (
-    local.core_configuration_to_write
-    ) : (
-    {
-      "${var.core_configuration_cluster_name}" = local.core_configuration_to_write
-    }
-  )
+  value = {
+    "${var.core_configuration_cluster_name}" = local.core_configuration_to_write
+  }
 }
