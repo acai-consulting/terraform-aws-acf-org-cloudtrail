d7b5f73c8d8e7c8402a0991011a321522874d97b
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-03-04 22:13:38 +0100

    fix

--- cicd-principals/main.tf
+++ cicd-principals/main.tf
@@ -9,27 +9,15 @@ data "template_file" "org_cloudtrail_admin" {
 data "template_file" "org_cloudtrail_bucket" {
   template = file("${path.module}/org_cloudtrail_bucket.yaml.tftpl")
   vars = {
-    target_principal_role_name = "var.target_principal_settings.target_principal_name"
-    target_principal_role_path = "var.target_principal_settings.role_path"
-    trustee_role_arn           = "var.target_principal_settings.pipeline_principal_arn"
-    resource_tags_block        = "local.resource_tags_rendered"
+    bucket_prefix                   = var.s3_bucket.bucket_name == null ? var.s3_bucket.bucket_prefix : var.s3_bucket.bucket_name
+    bucket_notification_to_sns_name = var.bucket_notification_to_sns.sns_name
   }
 }
 
-data "template_file" "bucket_notification_sns" {
-  template = file("${path.module}/bucket_notification_sns.yaml.tftpl")
-  vars = {
-    target_principal_role_name = "var.target_principal_settings.target_principal_name"
-    target_principal_role_path = "var.target_principal_settings.role_path"
-    trustee_role_arn           = "var.target_principal_settings.pipeline_principal_arn"
-    resource_tags_block        = "local.resource_tags_rendered"
-  }
-}
 
 output "cf_template_map" {
   value = {
-    "org_cloudtrail_admin.yaml.tftpl"    = replace(data.template_file.org_cloudtrail_admin.rendered, "$$$", "$$")
-    "org_cloudtrail_bucket.yaml.tftpl"   = replace(data.template_file.org_cloudtrail_bucket.rendered, "$$$", "$$")
-    "bucket_notification_sns.yaml.tftpl" = replace(data.template_file.bucket_notification_sns.rendered, "$$$", "$$")
+    "org_cloudtrail_admin.yaml.tftpl"  = replace(data.template_file.org_cloudtrail_admin.rendered, "$$$", "$$")
+    "org_cloudtrail_bucket.yaml.tftpl" = replace(data.template_file.org_cloudtrail_bucket.rendered, "$$$", "$$")
   }
 }
--- cicd-principals/variables.tf
+++ cicd-principals/variables.tf
@@ -6,3 +6,18 @@ variable "cloudwatch_loggroup" {
     iam_role_path = string
   })
 }
+
+variable "s3_bucket" {
+  description = "Configuration settings for core logging."
+  type = object({
+    bucket_name           = string
+    bucket_name_prefix    = string
+  })
+}
+
+variable "bucket_notification_to_sns" {
+  type = object({
+    sns_name            = string
+  })
+}
+
