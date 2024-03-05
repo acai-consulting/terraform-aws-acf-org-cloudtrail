3d8235b9c3d9c4475170512c6f407b57156f0f1b
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-03-05 18:19:45 +0100

    add output description

--- modules/log_archive/outputs.tf
+++ modules/log_archive/outputs.tf
@@ -1,11 +1,14 @@
 output "bucket_id" {
+  description = "Bucket ID of the CloudTrail bucket"
   value = aws_s3_bucket.cloudtrail_logs.id
 }
 
 output "bucket_arn" {
+  description = "ARN of the CloudTrail bucket"
   value = aws_s3_bucket.cloudtrail_logs.arn
 }
 
 output "kms_cmk_arn" {
+  description = "ARN of the KMS CMK for encrypting the logs"
   value = aws_kms_key.core_logging_cloudtrail_mgmt_kms.arn
 }
